df4a6edc00e4151a779bb46156933976102b9086
cgtucker <99846252+cgtucker@users.noreply.github.com>
2023-01-24 09:12:28 -0700

    made working files and turned them in to canvas for first assignment

--- src/ISBN.java
+++ src/ISBN.java
@@ -1,5 +1,32 @@
-public class Main {
+import java.util.Scanner;
+
+
+public class ISBN {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.print("Enter the first 9 digits of an ISBN: ");
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        int copyNum = num;
+        int totalNum = 0;
+        int quotient = 100_000_000;
+        int check = copyNum / quotient;
+        for (int i = 0; i < 9; i++){
+            int d = num / quotient;
+            totalNum += d * (i+1);
+            num -= d * quotient;
+            quotient /= 10;
+        }
+        int finalNum = totalNum % 11;
+        if (check == 0){
+            if (finalNum == 10){
+                System.out.print("The ISBN-10 number is: " + "0" + copyNum + "X");
+            }else{
+                System.out.print("The ISBN-10 number is: " + "0" + copyNum + finalNum);
+            }
+        } else if (finalNum == 10) {
+            System.out.print("The ISBN-10 number is: " + copyNum + "X");
+        }else {
+            System.out.print("The ISBN-10 number is: " + copyNum + finalNum);
+        }
     }
 }
--- src/Quadratic.java
+++ src/Quadratic.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Quadratic {
+import java.util.Scanner;
+
+public class Quadratic {
+    public static void main(String[] args) {
+        System.out.print("Enter a, b, c: ");
+        Scanner quadInput = new Scanner(System.in);
+        double a = quadInput.nextDouble();
+        double b = quadInput.nextDouble();
+        double c = quadInput.nextDouble();
+
+        double checkValid = Math.pow(b,2) - (4 * a * c);
+
+        if (checkValid>= 0){
+            double squareRoot = Math.sqrt(checkValid);
+            double r1 = ((-b + squareRoot)/2*a);
+            double r2 = ((-b - squareRoot)/2*a);
+            if (checkValid >0.0){
+                System.out.println("There are two roots for the quadratic equation with these coefficients.");
+                System.out.println("r1 ="+r1);
+                System.out.println("r2 ="+r2);
+        } else if (checkValid == 0.0) {
+                System.out.println("There is one root for the quadratic equation with these coefficients.");
+                System.out.println("r1 ="+r1);
+            }
+        } else if (checkValid < 0.0) {
+            System.out.println("There are no roots for the quadratic equation with these coefficients.");
+        }
+    }
 }
